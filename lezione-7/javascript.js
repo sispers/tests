30e1f8d46ce8c3d7e12bf6000f2e0c6267f3c1b5
Simone Pretto <simonepretto1999@gmail.com>
2024-08-23 09:02:18 +0200

    javascript-basics-2

---
+++ lezione-7/javascript.js
@@ -0,0 +1,37 @@
+//~ alt 126
+//`` alt 96
+
+var gladiator = {
+    gladiator_name: `Pippo`,
+    gladiator_age: 28,
+    total_def: 10,
+    base_def: 20,
+    armor: 20,
+    shield: 8,
+    atk: {
+        base_atk: 10,
+        weapon: `longsword`,
+    },
+    
+};
+
+var message = `the defense of "${gladiator.gladiator_name}" is "${gladiator.total_def}"`;
+console.log (message);
+
+
+var gladiator_def = (a, b, c) => {
+    var total_def = (a.base_def + b.armor + c.shield);
+    // total_def = (gladiator, gladiator, gladiator);
+    gladiator_def = (gladiator.base_def, gladiator.armor, gladiator.shield);
+
+    
+    var message = `the defense of "${gladiator.gladiator_name}" is "${gladiator.total_def}"`;
+    console.log (message);
+};
+
+
+var sum = (a, b) => {
+    console.log(a +b);
+};
+
+sum(5, 6);
